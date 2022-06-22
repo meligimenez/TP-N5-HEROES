62067149b58c9bee7eba216dc1cb16a37f1eb643
Meli Gimenez <melinagimenez1999@gmail.com>
2022-06-22 11:53:45 -0300

    Poniendo mi repo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
